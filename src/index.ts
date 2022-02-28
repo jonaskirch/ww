d43d5d98acca871fb86039875270bdd51a96570c
jonaskirch <jonaskirch9@gmail.com>
2022-02-28 11:46:09 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import qrcode from 'qrcode-terminal';
 
-import { Client } from 'whatsapp-web.js';
+import { Client, LocalAuth, LegacySessionAuth, NoAuth } from 'whatsapp-web.js';
+import { saveSession, getSession } from './sessionPersist';
 
 const client = new Client({
+  // session: getSession(),
+  authStrategy: new LegacySessionAuth({
+    session: getSession(),
+  }),
   puppeteer: {
     // headless: true,
     // timeout: 99999999,
@@ -25,14 +30,16 @@ client.on('ready', () => {
   console.log('Client is ready!');
 });
 
+client.on('authenticated', session => {
+  saveSession(session);
+});
+
 client.on('message', message => {
   // console.log('message',message);
   console.log('msg', message.body);
-  if (message.body == 'ping') {
+  if (message.body === 'ping') {
     client.sendMessage(message.from, 'pong');
   }
 });
 
 client.initialize();
-
-console.log('fim 2');
---
+++ src/sessionPersist.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import { ClientSession } from 'whatsapp-web.js';
+
+const SESSION_FILE_PATH = './sessionData.json';
+
+function saveSession(session: ClientSession | undefined) {
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+function getSession(): ClientSession | undefined {
+  let sessionData = '';
+  try {
+    sessionData = fs.readFileSync(SESSION_FILE_PATH).toString();
+  } catch (e) {
+    console.log('Session data file not load');
+  }
+  if (sessionData) {
+    const session: ClientSession = JSON.parse(sessionData);
+    return session;
+  }
+  return undefined;
+}
+
+export { saveSession, getSession };
